e8d35b2ee10d6205a83b666e4070deea532079d5
Walter Wu <walter.wu@berkeley.edu>
2016-04-22 14:33:15 -0700

    added a input generator, and also the generated INPUT. This is a 5---vertex graph where each vertex has a directed edge towards every other vertex. Also, all even-numbered vertices are childeren

---
+++ input1_generator.py
@@ -0,0 +1,10 @@
+output = open("TESTINSTANCE1.in", "w")
+output.write("500\n")
+for node in range(0, 500, 2):
+	output.write(str(node) + " ")
+output.write("\n")
+for node_a in range(0, 500):
+	for node_b in range(0, 500):
+		if node_a != node_b:
+			output.write("1 ")
+	output.write("\n")
