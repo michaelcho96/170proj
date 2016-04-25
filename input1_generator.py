e4914498f1ef8a6bfc3226651a8eda6bcad8f295
Walter Wu <walter.wu@berkeley.edu>
2016-04-24 21:59:19 -0700

    added description to this output

--- input1_generator.py
+++ input1_generator.py
@@ -1,3 +1,10 @@
+# This creates a graph where every vertex has a directed edge
+# to every other vertex. This is a hard input for very naive 
+# solutions.
+# While the concept may be used, this IN NO WAY a submittable
+# input. Any half-assed algorithm will immediatly negate all
+# pairs and return the optimal solution in no time at all. 
+
 output = open("TESTINSTANCE1.in", "w")
 output.write("500\n")
 for node in range(0, 500, 2):
