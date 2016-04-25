fecc8178c9c6582f277f21a52f5bb45b0a435c51
Walter Wu <walter.wu@berkeley.edu>
2016-04-25 13:20:36 -0700

    removed all child nodes

--- input1_generator.py
+++ input1_generator.py
@@ -7,8 +7,7 @@
 
 output = open("TESTINSTANCE1.in", "w")
 output.write("500\n")
-for node in range(0, 500, 2):
-	output.write(str(node) + " ")
+# We have no child nodes.
 output.write("\n")
 for node_a in range(0, 500):
 	for node_b in range(0, 500):
