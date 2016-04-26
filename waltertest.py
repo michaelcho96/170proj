9d67fd68e6ecf6c7077c7f2b5e747c77245cc2af
Walter Wu <walter.wu@berkeley.edu>
2016-04-25 21:22:07 -0700

    removed duplicate files

--- waltertest.py
+++
@@ -1,8 +0,0 @@
-import networkx as nx
-
-def make_graph(number_nodes):
-	G = nx.Graph()
-	for node in range(0, number_nodes):
-		G.add_node(node)
-	print("Number of nodes: %s" %(G.number_of_nodes()))
-make_graph(4)
