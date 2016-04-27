74cc40c1a8df8cc41502361ec7383769885396f8
Walter Wu <walter.wu@berkeley.edu>
2016-04-27 14:08:51 -0700

    removed wrong utils

--- utils.py
+++ utils.py
@@ -28,3 +28,30 @@ def create_graph(instance):
                 if elements[j] == "1":
                     g.add_edge(i,j)
     return g
+
+def find_cycles(input_graph):
+    nodes = input_graph.nodes();
+    visited = []
+    for node in nodes:
+        visited[node] = False
+    visited[0] = True
+
+def explore(root_node, node, input_graph, cycle):
+    # At each vertex, we check to see if any of the edges lead back to the root
+    # node. If so, we add that cycle to our list of cylces. If there are edges
+    # which do not lead back to the vertex, we explore those nodes as well. 
+    # Once we have searched all other edges, we return the list of cycles.
+
+    #If the cycle has five elements in it, it can no longer qualify
+    if (len(cycle) == 5):
+        return [] 
+    cycle_list = []
+    for edge in input_graph.edges(node, False):
+        next_vertex = edge[1]
+        if edge[1] = root_node:
+            cycle_list.append(cycle)
+        else:
+            updated_cycle = cycle
+            updated_cycle.append[next_vertex]
+            cycle_list.extend(explore(root_node, next_vertex, input_graph, updated_cycle))
+        return cycle_list 
