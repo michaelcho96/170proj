151e991d00fc7a9e839aa34aa746bb06e601dd6e
Michael Cho <michaelcho96@berkeley.edu>
2016-04-27 13:49:41 -0700

    updated parser again

--- Part1/parse_instance.py
+++ Part1/parse_instance.py
@@ -6,16 +6,21 @@ def create_graph(instance):
     Parses file, returns a networkx Digraph object
     Nodes have 'penalty' attributes: 1 for adults, 2 for children.
     """
+    print("creating graph of " + instance)
     with open(instance) as f:
         n = int(f.readline())
-        children = f.readline().split(" ")
+        children_line = f.readline()
+        if children_line != "\n":
+            children = children_line.split(" ")
+        else: 
+            children = []
         g = nx.DiGraph()
-        g.add_nodes_from([0,n], penalty=1)
+        g.add_nodes_from(range(n), penalty=1)
         for c in children:
             if type(c) == int:
                 g.node[c]['penalty'] = 2
             elif type(c) == str:
-                g.node[int(c)]['penalty'] = 2
+                g.node[int(c.strip())]['penalty'] = 2
         for i in range(n):
             row = f.readline()
             elements = row.split(" ")
