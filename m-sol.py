a00970eb26c814f67b8f4905c9147c32efc8f2e0
Michael Cho <michaelcho96@berkeley.edu>
2016-04-29 00:01:28 -0700

    added simple_cycles to utils

--- m-sol.py
+++ m-sol.py
@@ -1,7 +1,7 @@
 import sys
 from utils import *
 import networkx as nx
-
+import datetime
 
 # def naive_sol(g, cycles):
 #     """
@@ -25,7 +25,6 @@ import networkx as nx
 #         sols.add(g, naive_sol(g_copy, cycles.append(cycle)))
 #     return min(sols, key=sols.get)
 
-# @timeout()
 def naive_sol(g, cycles, all_cycles):
     """
     In progress
@@ -101,43 +100,91 @@ def all_short_cycles(g):
 #             outf.write(write_str + "\n")
 
 
-def find_all_sol(outfile):
-    with open(outfile, 'w') as outf:
-            for i in range(1,493):
-                sol = single_sol(i)
-                outf.write(sol + "\n")
+def find_all_sol(outfile, logfile):
 
+    for i in range(1,493):
+        sol, log = single_sol(i)
+        with open(outfile, 'a') as outf:
+            outf.write(sol + "\n")
+        with open(logfile, 'a') as logf:
+            logf.write(log + "\n")
+
+
+@timeout(120)
 def single_sol(inst):
+    log_str = ""
     if type(inst) is int:
         num = str(inst)
         in_file = "instances/{0}.in".format(num)
-        print("Creating graph of instance {0}".format(num))
+        update = "Creating graph of instance {0}".format(num)
+        print(update)
+        log_str += update + "\n"
     elif type(inst) is str:
         in_file = inst
-        print("Creating graph of {0}".format(in_file))
+        update = "Creating graph of {0}".format(in_file)
+        print(update)
+        log_str += update + "\n"
     else:
         raise TypeError
     g = create_graph(in_file)
     num_edges = len(nx.edges(g))
-    print("Graph has {0} edges...".format(str(num_edges)))
+    update = "Graph has {0} edges...".format(str(num_edges))
+    print(update)
+    log_str += update + "\n"
     write_str = "Skipped"
     if len(nx.edges(g)) < 1000:
-        print("Finding short cycles...")
+        update = "Finding short cycles..."
+        print(update)
+        log_str += update + "\n"
         try:
             all_cycles = all_short_cycles(g)
-            print("Running algorithm on {0} cycles".format(len(all_cycles)))
+            if len(all_cycles) > 20:
+                raise TimeoutError
+            update = "Running algorithm on {0} cycles...".format(len(all_cycles))
+            print(update)
+            log_str += update + "\n"
             cost, cycles = naive_sol(g, [], all_cycles)
             if cycles == []:
                 write_str = "{0}       None".format(str(cost))
             else: 
                 write_str = "{0}       {1}".format(str(cost), format_output_cycles(cycles))
         except(TimeoutError):
-            print("Timeout!")
-    else:
-        print("Skipping!")
-    print(write_str + "\n")
-    return write_str
+            update = "Timeout!"
+            print(update)
+            log_str += update + "\n"
+    else: 
+        update = "Skipping!"
+        print(update)
+        log_str += update    
+    update = str(inst) + write_str + "\n"
+    update = "Timeout!"
+    print(update + "\n")
+    log_str += update + "\n"
+    return write_str, log_str
 
-find_all_sol("m_all_out2.txt")
 
-# single_sol(120)
+# date_suffix = datetime.now()
+# find_all_sol("m_all_out + {0}.txt".format(date_suffix, "m_log + {0}.txt".format(date_suffix))
+
+def single_sol_by_input():
+    inp = input("Enter instance number, or None: ")
+    if inp != "None":
+        single_sol(int(inp))
+
+# single_sol_by_input()
+
+
+
+def sol_by_range(start, end):
+    if start not in range(0,493):
+        raise ValueError("Start not in range")
+    if end not in range(0,493):
+        raise ValueError("End not in range")
+    if start >= end:
+        raise ValueError("Start must be before end")
+    with open("m_out_{0}-{1}.txt".format(str(start), str(end)), "w") as outf:
+        for i in range(start, end + 1):
+            sol, log = single_sol(i)
+            outf.write(sol + "\n")
+
+sol_by_range(1,10)
