269a2580be6974b8909e3c2270ad6a8953afe64b
Walter Wu <walter.wu@berkeley.edu>
2016-04-30 17:03:30 -0700

    ran greedy on all instances, cutting off after 5 seconds.

--- greedy_solution.py
+++ greedy_solution.py
@@ -9,13 +9,14 @@ import time
 
 def greedy_algorithm(G):
 	CG = construct_cluster_graph(G)
+	CG_copy = CG.copy()
 	valid_nodes = CG.nodes()
-	selected_nodes = []
+	selected_clusters = []
 	pre_penalty = 0
 	while len(valid_nodes) != 0:
 		node_selected = find_max_penalty(valid_nodes, CG)
 		pre_penalty += CG.node[node_selected]['penalty']
-		selected_nodes.append(node_selected)
+		selected_clusters.append(node_selected)
 		list_edges = CG.edges(node_selected, False)
 		nodes_to_remove = set()
 		for edge in list_edges:
@@ -23,11 +24,10 @@ def greedy_algorithm(G):
 			nodes_to_remove.add(edge[1])
 		for node in nodes_to_remove:
 			valid_nodes.remove(node)
+			CG.remove_node(node)
 	list_cycles = []
-	for node in selected_nodes:
-		cycle = []
-		cycle.append(CG.node[node]['nodes'])
-		list_cycles.append(cycle)
+	for cluster in selected_clusters:
+		list_cycles.append(CG_copy.node[cluster]['nodes'])
 	penalty = find_total_penalty(G) - pre_penalty
 	output_string = format_output_cycles(list_cycles)
 	return [output_string, penalty]
@@ -53,18 +53,19 @@ def execute_greedy(index):
 def timed_execution():
 	if __name__ == '__main__':
     	# Start foo as a process
-		for index in range(1,3):
+		for index in range(85,493):
 			print("Processing input " + str(index) + ".")
 			p = multiprocessing.Process(target=execute_greedy, name="execute_greedy", args=(index,))	
 			p.start()
  			# Wait 10 seconds for foo
-			time.sleep(120)
+			time.sleep(5)
 			if p.is_alive():
 				print("Process " + str(index) + " still running. Killing.")
 				p.terminate()
 				# Cleanup
 				p.join()
 			p.join()
+
 timed_execution()
 
 
