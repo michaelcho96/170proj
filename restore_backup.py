d9bbd7f004985f7a743abbd724324090a32efeef
Walter Wu <walter.wu@berkeley.edu>
2016-05-02 10:30:58 -0700

    results for randon()

--- restore_backup.py
+++ restore_backup.py
@@ -3,5 +3,5 @@ import io
 from shutil import copyfile
 
 filename = "SOLUTIONS RECORDS.txt"
-backup_filename = "SOLUTIONS RECORDS.bak"
+backup_filename = "SOLUTIONS RECORDS - M.txt"
 copyfile(backup_filename, filename)
