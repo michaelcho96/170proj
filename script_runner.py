7afecfdc19aee2dee7aa32148fd0f570d3db2590
Walter Wu <walter.wu@berkeley.edu>
2016-05-02 13:18:31 -0700

    updated condensed solutions

--- script_runner.py
+++ script_runner.py
@@ -1,5 +1,7 @@
 from utils import combine
+from utils import write_condensed_solutions
 
-file_a = "UPDATED BEST RANDOM.txt"
-file_b = "SOLUTIONS RECORDS.txt"
-combine(file_a, file_b)
+#file_a = "UPDATED BEST RANDOM.txt"
+#file_b = "SOLUTIONS RECORDS.txt"
+#combine(file_a, file_b)
+write_condensed_solutions("COMBINED SOLUTIONS")
