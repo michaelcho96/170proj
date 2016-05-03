e262b57ff6d78bb1f90d6a92ab28cfe5636c4783
Michael Cho <michaelcho96@berkeley.edu>
2016-05-02 18:59:04 -0700

    pushing new updated sol

--- execute_random2.py
+++ execute_random2.py
@@ -32,32 +32,32 @@ def run_on_all_instances(name):
 	sol_list = []
 	graphs = {}
 
-	# copyfile("COMBINED SOLUTIONS", file_a)
-	# copyfile("COMBINED SOLUTIONS", file_b)
+	copyfile("COMBINED SOLUTIONS", file_a)
+	copyfile("COMBINED SOLUTIONS", file_b)
 
 	try:
 		i+= 1
 		print("Starting iteration {0}...".format(str(i)))
 		print("Getting solved instances...")
-		solved = write_zero_penalty_list(file_a)
+		solved = do_not_test_set(file_a)
 		print(solved)
-		print("{0} instances with penalty 0!".format(str(len(solved))))
-		# for instance in range(1, 493):
-		# 	if str(instance) not in solved:
-		# 		print("\nProcessing input {0} on iteration {1}".format(str(instance), str(i)))
-		# 		min_penalty = 9999
-		# 		curr_best = None
-		# 		for index in range (0,30):
-		# 			print(".", end="")
-		# 			form_sol, graphs = execute_random2(instance, graphs)
-		# 			penalty = form_sol[2] 
-		# 			if penalty < min_penalty:
-		# 				min_penalty = penalty
-		# 				curr_best = form_sol
-		# 				if min_penalty == 0:
-		# 					print("\n Found a 0!")
-		# 					break
-		# 		add_solutions([curr_best,], file_b)
+		print("{0} solved instances!".format(str(len(solved))))
+		for instance in range(1, 493):
+			if instance not in solved:
+				print("\nProcessing input {0} on iteration {1}".format(str(instance), str(i)))
+				min_penalty = 9999
+				curr_best = None
+				for index in range (0,30):
+					print(".", end="")
+					form_sol, graphs = execute_random2(instance, graphs)
+					penalty = form_sol[2] 
+					if penalty < min_penalty:
+						min_penalty = penalty
+						curr_best = form_sol
+						if min_penalty == 0:
+							print("\n Found a 0!")
+							break
+				add_solutions([curr_best,], file_b)
 		print("Updating {0} with {1}...".format(file_a, file_b))
 		updated_instances += update_sol_list(file_a, file_b)
 		print("Updating condensed solutions...")
