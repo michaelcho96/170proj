b6ec9b7b67aa19e96c61b40a925c59664fc3780d
Walter Wu <walter.wu@berkeley.edu>
2016-05-02 17:14:34 -0700

    updated solutions.out

--- combine_solutions.py
+++ combine_solutions.py
@@ -1,7 +1,7 @@
 from utils import combine
 import sys
 import os.path
-import io
+from utils import read_solution_line
 from shutil import copyfile
 
 if len(sys.argv) != 2:
@@ -11,4 +11,18 @@ else:
 	combine("COMBINED SOLUTIONS", file)
 	copyfile("COMBINED SOLUTIONS", "CURRENT BEST SOLUTIONS")
 	print("Copied new solutions to CURRENT BEST SOLUTIONS.")
+	print("Done.")
+	# Writing to solutions.out
+	print("Writing to solutions.out")
+	write_file = open("solutions.out", "w")
+	read_file = open("COMBINED SOLUTIONS", "r")
+	data = read_file.readlines()
+	for line in data:
+		info = read_solution_line(line)
+		solution = info[3]
+		if solution.isspace():
+			write_file.write("None\n")
+		else:
+			solution = solution[1:]
+			write_file.write(solution + "\n")
 	print("Done.")
